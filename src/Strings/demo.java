20e88f3e9aee0b13dba4a8c2cb9d8a8f889bf1de
Leon-S-Kennedy <937854328@qq.com>
2022-04-14 13:49:13 +0800

    string

---
+++ src/Strings/demo.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class demo{
+    public static void main(String[] args) {
+        String s1="abc"+"def";//1
+        String s2=new String(s1);//2
+        if(s1.equals(s2))//3
+            System.out.println(".equals succeeded");//4
+        if(s1==s2)//5
+            System.out.println("==succeeded");//6
+
+
+    }
+}
